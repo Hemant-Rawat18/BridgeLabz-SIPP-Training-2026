f3f5d8eae20c6802993f246d7787d321188309a4
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-07-06 11:55:52 +0530

    [128] [Time Beats: 95.96%] [Memory Beats: 98.38%] - LeetPush

--- 100/128-Longest-Consecutive-Sequence.java
+++ 100/128-Longest-Consecutive-Sequence.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int longestConsecutive(int[] arr) {
-        if(arr.length==0)return 0;
-        Arrays.sort(arr);
+    public int longestConsecutive(int[] nums) {
+        Arrays.sort(nums);
+        if(nums.length==0)return 0;
         int count=1;
         int max=1;
-        for(int i=1;i<arr.length;i++){
-             if(arr[i]==arr[i-1]){
-                    continue;
-                }
-             else if(arr[i]-arr[i-1]==1){
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]==nums[i]){
+                continue;
+            }else if(nums[i]-nums[i-1]==1){
                 count++;
-               
+                max=Math.max(count,max);
             }
-            else {
+           
+            else{
                 count=1;
             }
-            max=Math.max(count,max);
         }
         return max;
     }
