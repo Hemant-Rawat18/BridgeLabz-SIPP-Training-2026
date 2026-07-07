7609e3713593d3b6bd7c8f50fd8d62ec97a78fb0
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-07-07 18:05:04 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 10.68%] - LeetPush

--- 100/75-Sort-Colors.java
+++ 100/75-Sort-Colors.java
@@ -1,5 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
+         int arr[]=new int[nums.length];
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                arr[k++]=nums[i];
+
+            }
+        }
+         for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                arr[k++]=nums[i];
+
+            }
+        }
+         for(int i=0;i<nums.length;i++){
+            if(nums[i]==2){
+                arr[k++]=nums[i];
+
+            }
+        }
+         for(int i=0;i<nums.length;i++){
+           nums[i]=arr[i];
+            
+        }
         
     }
 }
