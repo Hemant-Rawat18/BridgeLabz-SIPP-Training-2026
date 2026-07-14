5a4b3d3c327b02af612b2c0fc7ad9432d288e42e
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-07-14 10:05:32 +0530

    [1941] [Time Beats: 19.78%] [Memory Beats: 38.26%] - LeetPush

---
+++ 100/1941-Check-if-All-Characters-Have-Equal-Number-of-Occurrences.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+        HashMap<Character,Integer>hm=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+        }
+         int freq = hm.get(s.charAt(0));
+
+        for (char ch : hm.keySet()) {
+            if (hm.get(ch) != freq) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
