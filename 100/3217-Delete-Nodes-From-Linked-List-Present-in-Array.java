8de695ca57da939caccf2fd0cb3d75446951fabe
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-07-26 10:25:36 +0530

    [3217] [Time Beats: 87.53%] [Memory Beats: 25.64%] - LeetPush

---
+++ 100/3217-Delete-Nodes-From-Linked-List-Present-in-Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int x : nums) {
+            set.add(x);
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = dummy;
+        while (curr.next != null) {
+            if (set.contains(curr.next.val)) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+        return dummy.next;
+    }
+}
