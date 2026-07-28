2482e1b31c1e1d7de7bc449f569af43faf6f4591
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-07-28 19:00:42 +0530

    [1004] [Time Beats: 96.01%] [Memory Beats: 36.20%] - LeetPush

--- 100/1004-Max-Consecutive-Ones-III.java
+++ 100/1004-Max-Consecutive-Ones-III.java
@@ -1,25 +1,26 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int count=0;
-        int max=0;
-        for(int i=0;i<nums.length;i++){
-            count=0;
-            int temp=k;
-
-            for(int j=i;j<nums.length;j++){
-                if(nums[j]==1){
-                    count++;
-                }else{
-                    if(temp>0){
-                        count++;
-                        temp--;
-                    }else{
-                        break;
-                    }
-                }
+        int C1=0;
+        int C0=0;
+        int l=0;
+        int r=0;
+        int maxwindow=0;
+        for(r=0;r<nums.length;r++){
+            if(nums[r]==0){
+                C0++;
             }
-            max=Math.max(count,max);
+            while(C0>k){
+                if(nums[l]==0){
+                C0--;
+                }
+                l++;
+
+            }
+           int  length=r-l+1;
+            maxwindow=Math.max(maxwindow,length);
         }
-        return max;
+        return maxwindow;
+
+        
     }
 }
