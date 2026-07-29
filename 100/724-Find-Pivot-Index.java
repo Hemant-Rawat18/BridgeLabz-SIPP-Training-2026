af615f90c1b397451ab9241747cf5bb58d94d7f6
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-07-29 06:29:54 +0530

    [724] [Time Beats: 97.63%] [Memory Beats: 22.81%] - LeetPush

---
+++ 100/724-Find-Pivot-Index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+        }
+        int temp=0;
+        for(int i=0;i<nums.length;i++){
+            temp+=nums[i];
+            if(sum==temp){
+                return i;
+            }else{
+                sum=sum-nums[i];
+            }
+        }
+        return -1;
+    }
+}
