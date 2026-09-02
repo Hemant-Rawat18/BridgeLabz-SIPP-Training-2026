0f5b7eecf46ba86365297868040a457948880ebd
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-09-02 11:34:53 +0530

    [58] [Time Beats: -%] [Memory Beats: -%] - LeetPush

--- 100/58-Length-of-Last-Word.java
+++ 100/58-Length-of-Last-Word.java
@@ -2,10 +2,10 @@ class Solution {
     public int lengthOfLastWord(String s) {
         s=s.trim();
         int count=0;
-       for(int i=s.length()-1;i>=0;i--){
-        if(s.charAt(i)==' ')break;
-        count++;
-       }
-       return count;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)==' ')break;
+            count++;
+        }
+        return count;
     }
 }
