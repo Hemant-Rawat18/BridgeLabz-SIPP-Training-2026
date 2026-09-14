b498f32d89653833f9702d572caa3a3aa7cd7242
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-09-14 08:20:43 +0530

    [58] [Time Beats: 100.00%] [Memory Beats: 51.21%] - LeetPush

--- 100/58-Length-of-Last-Word.java
+++ 100/58-Length-of-Last-Word.java
@@ -3,8 +3,11 @@ class Solution {
         s=s.trim();
         int count=0;
         for(int i=s.length()-1;i>=0;i--){
-            if(s.charAt(i)==' ')break;
-            count++;
+            if(s.charAt(i)==' '){
+                break;
+            }else{
+                count++;
+            }
         }
         return count;
     }
