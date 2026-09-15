a632659840e23d8c489b17e1035231372628e5f2
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-09-15 09:15:03 +0530

    [49] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 100/49-Group-Anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>>hm=new HashMap<>();
+        for(String s:strs){
+            char c[]=s.toCharArray();
+            Arrays.sort(c);
+            String key =new String(c);
+            if(!hm.containsKey(key)){
+                hm.put(key,new ArrayList<>());
+            }
+            hm.get(key).add(s);
+        }
+        return new ArrayList<>(hm.values());
+    }
+}
